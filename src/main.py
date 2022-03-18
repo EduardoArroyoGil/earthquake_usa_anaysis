a76c6b1ab47dd52b122e1d616ef658ee81412511
EduardoArroyoGil <eduardoarroyogil@gmail.com>
2022-03-18 07:28:03 +0100

    analysis module, images analysis and notebook for analysis

--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
 import sys
 import os
+sys.path.append("../")
 import time
 import src.generates_csv as gcsv
 import src.gdp_states_usa as gstatesusa
 import src.generate_final_data as gfinaldata
+import src.analysis as analysis
 
-sys.path.append("../")
 
 sys.stdout = open("log_main_earthquake_vs_gdp.txt", "w")
 
 print('START MAIN PROCEDURE\n')
 general_start = time.process_time()
 
-
 gcsv.generate_csv()
 gstatesusa.generate_gdp_state()
 gfinaldata.generate_final_data()
+analysis.analysis()
 
-duration = round((time.process_time() - general_start)/60,2)
+duration = round((time.process_time() - general_start)/60, 2)
 print(f'FINISH MAIN PROCEDURE (general duration: {duration} min)')
 
 sys.stdout.close()
